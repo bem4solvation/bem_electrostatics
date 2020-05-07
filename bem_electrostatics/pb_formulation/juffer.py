954bcf585f333e71730211599c862751706f4945
Stefan Search <stefan.search.14@sansano.usm.cl>
2020-05-07 12:42:29 -0400

    Correct error in juffer formulation. Tidy code to refelct notation.

--- bem_electrostatics/pb_formulation/juffer.py
+++ bem_electrostatics/pb_formulation/juffer.py
@@ -10,7 +10,7 @@ def juffer(dirichl_space, neumann_space, q, x_q, ep_in, ep_ex, kappa):
 
     dF = laplace.double_layer(dirichl_space, dirichl_space, dirichl_space)
     dP = modified_helmholtz.double_layer(dirichl_space, dirichl_space, dirichl_space, kappa)
-    L1 = ep*dP - dF
+    L1 = (ep*dP) - dF
 
     F = laplace.single_layer(neumann_space, dirichl_space, dirichl_space)
     P = modified_helmholtz.single_layer(neumann_space, dirichl_space, dirichl_space, kappa)
@@ -22,28 +22,26 @@ def juffer(dirichl_space, neumann_space, q, x_q, ep_in, ep_ex, kappa):
 
     dF0 = laplace.adjoint_double_layer(neumann_space, neumann_space, neumann_space)
     dP0 = modified_helmholtz.adjoint_double_layer(neumann_space, neumann_space, neumann_space, kappa)
-    L4 = dF0 - (1./ep)*dP0
+    L4 = dF0 - ((1.0/ep)*dP0)
 
-    blocked = bempp.api.BlockedOperator(2, 2)
-    blocked[0, 0] = 0.5*(1. + ep)*phi_id - L1
-    blocked[0, 1] = -L2
-    blocked[1, 0] = L3    # Cambio de signo por definicion de bempp
-    blocked[1, 1] = 0.5*(1. + 1./ep)*dph_id - L4
-    #A = blocked.strong_form()
-    A = blocked
+    A = bempp.api.BlockedOperator(2, 2)
+    A[0, 0] = (0.5*(1.0 + ep)*phi_id) - L1
+    A[0, 1] = (-1.0)*L2
+    A[1, 0] = L3    # Cambio de signo por definicion de bempp
+    A[1, 1] = (0.5*(1.0 + (1.0/ep))*dph_id) - L4
 
     @bempp.api.real_callable
     def d_green_func(x, n, domain_index, result):
         nrm = np.sqrt((x[0]-x_q[:,0])**2 + (x[1]-x_q[:,1])**2 + (x[2]-x_q[:,2])**2)
         
         const = -1./(4.*np.pi*ep_in)
-        result[:] = (-1.0)*const*np.sum(q*np.dot(x-x_q, n)/(nrm**3))
+        result[:] = const*np.sum(q*np.dot(x-x_q, n)/(nrm**3))
 
     @bempp.api.real_callable
     def green_func(x, n, domain_index, result):
         nrm = np.sqrt((x[0]-x_q[:,0])**2 + (x[1]-x_q[:,1])**2 + (x[2]-x_q[:,2])**2)
         
-        result[:] = (-1.0)*np.sum(q/nrm)/(4.*np.pi*ep_in)
+        result[:] = np.sum(q/nrm)/(4.*np.pi*ep_in)
 
         
     rhs_1 = bempp.api.GridFunction(dirichl_space, fun=green_func)
