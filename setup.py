9845330d0c613ff7d64da87c08140ca2b79297b9
Stefan Search <stefan.search@gmail.com>
2021-08-21 14:30:43 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='bem_electrostatics',
+    version='',
+    packages=['bem_electrostatics', 'bem_electrostatics.utils', 'bem_electrostatics.openmm',
+              'bem_electrostatics.mesh_tools', 'bem_electrostatics.pb_formulation',
+              'bem_electrostatics.pb_formulation.formulations'],
+    url='',
+    license='',
+    author='Stefan',
+    author_email='stefan.search.14@sansano.usm.cl',
+    description='Python library for use with the bempp-cl library, for the solving of the PB implicit solvent model using BEM.'
+)
